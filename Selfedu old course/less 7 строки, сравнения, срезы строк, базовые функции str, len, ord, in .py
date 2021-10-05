d976e06741cbc58812ccb9262f3ae1fdb6981834
Zaharov Andrei <redzed427@gmail.com>
2021-10-05 06:45:32 +0300

    'Py-course'

--- Selfedu old course/less 7 строки, сравнения, срезы строк, базовые функции str, len, ord, in .py
+++ Selfedu old course/less 7 строки, сравнения, срезы строк, базовые функции str, len, ord, in .py
@@ -1,15 +1,23 @@
 #Первый пример
 
-psw = "pass"
-in_psw = ""
-while psw != in_psw:
-    in_psw = input("Введите пароль: ")
-print("Вход в систему разрешен")
+#psw = "pass"
+#in_psw = ""
+#while psw != in_psw:
+    #in_psw = input("Введите пароль: ")
+#print("Вход в систему разрешен")
 
 #Задания к уроку
 
 
+#Задача №3 (Полиндром или нет) 
+#str=input("Enter word: "); z=len(str)
+#if  str[0::1]==str[z::-1]:
+    #print("yes")
+#else:
+      #print("no")
 
-
-
+#Задание 2) a = "abracadabra"  удалить все сочитания ab
+a = "abracadabra"
+b = a.count('ab')
+print(a.replace("ab","",b))
 
