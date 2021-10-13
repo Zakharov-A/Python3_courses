5ce17982811bf9d4c01aef840b8eabc8870bb4db
RedZed <global user.email redzed427@gmail.com>
2021-10-13 10:06:14 +0300

    'Py_course'

--- Selfedu_old_course/less 8 методы строк upper, lower, count, rfind, index, replace, split, join, find, strip, isalpha, isdigit, rjust, ljust, rstrip, lstrip.py
+++ Selfedu_old_course/less 8 методы строк upper, lower, count, rfind, index, replace, split, join, find, strip, isalpha, isdigit, rjust, ljust, rstrip, lstrip.py
@@ -1,8 +1,27 @@
+#Метод строки count
+
 #msg = "abrakadabra"
 #co = msg.count("ra")
 #print(co)
+
+#Метод строки replace
+
+#msg = "abrakadabra"
 #co = msg.replace("ab", "")
 #print(co)
+
+#a = "2+3+6.7+82+5.7+1"
+#d = a.replace("+", "-")
+#print(d)
+
+#Метод строки count
+
+#msg = "anananalala"
+#sdf = msg.count('na')
+#ert = msg.find('la')
+#print(sdf, ert)
+
+#Метод строки isgigit
 
 #dig = input("Введите число: ")
 #if(dig.isdigit()):
@@ -11,6 +30,7 @@
 #else:
     #print("Число введено неверно")
 
-a = "2+3+6.7+82+5.7+1"
-d = a.replace("+", "-")
-print(d)
+#Задания:
+
+
+
