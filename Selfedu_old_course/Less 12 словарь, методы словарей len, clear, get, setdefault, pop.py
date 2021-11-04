e4cbc621e02b7b1cad3aa81f1fb9defa4421448e
root <Zakharov-A>
2021-11-04 09:19:54 -0400

    Py_course_selfedu_12

--- Selfedu_old_course/Less 12 словарь, методы словарей len, clear, get, setdefault, pop.py
+++ Selfedu_old_course/Less 12 словарь, методы словарей len, clear, get, setdefault, pop.py
@@ -1,8 +1,52 @@
+#d = {"house": "дом", "car": "машина",
+     #"tree": "дерево", "river": "река",
+     #"road": "дорога"}
+#print(d)
+#print(d["house"])
+
+
+#d2 = dict(house = "дом", car = "машина",
+          #tree = "дерево", road = "дорога",
+          #river = "река")
+#print(d2)
+
+#a = dict.fromkeys(["+7", "+6", "+5", "+4"], "Code of the country")
+#print(a)
+
+
+#d = {True: 1, False: "Ложь", "list": [1,2,3], 5: 5}
+#for x in d:
+ #print(x, d[x])
+
+
+#d = {True: 1, False: "Ложь", "list": [1,2,3], 5: 5}
+#d.setdefault(3, "three")
+#print(d)
+#d2 = d.pop(3)
+#print(d2)
+
+#d = {True: 1, False: "Ложь", "list": [1,2,3], 5: 5}
+#for x in d.items():
+    #print(x[0], x[1])
+
+
+#d = {True: 1, False: "Ложь", "list": [1,2,3], 5: 5}
+#for x in d.keys():
+ #print(x, d[x])
+
 #d = {True: 1, False: "Ложь", "list": [1,2,3], 5: 5}
 #for x in d.values():
-    #print(x)
+ #print(x)
+
+
+#d = {True: 1, False: "Ложь", "list": [1,2,3], 5: 5}
+#for x in d.items():
+ #print(x[0], x[1])
 
 d = {True: 1, False: "Ложь", "list": [1,2,3], 5: 5}
-for x in d.items():
-    print(x[0], x[1])
+for key, value in d.items():
+ print(key, value)
 
+#Задания
+ 
+ 
