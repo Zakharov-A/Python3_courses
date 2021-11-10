c5e646103001c7637efeab12624791c9fb0ecbf2
root <Zakharov-A>
2021-11-10 12:07:48 -0500

    My_Py_Selfedu_13_Tasks

--- Selfedu_old_course/Less 13 кортежи tuple и операции с ними len, del, count, index.py
+++ Selfedu_old_course/Less 13 кортежи tuple и операции с ними len, del, count, index.py
@@ -7,15 +7,15 @@
 
 #решение задачи 2 урока 13
 
-a = "Оценки: 5, 4, 3, 4, 2, 4, 5, 4"
-b = a.split(": ")[1].split(', ')
-d = []
-c = tuple(b)
-for i in c:
-    d.append(int(i))
-f = tuple(d)
+#a = "Оценки: 5, 4, 3, 4, 2, 4, 5, 4"
+#b = a.split(": ")[1].split(', ')
+#d = []
+#c = tuple(b)
+#for i in c:
+    #d.append(int(i))
+#f = tuple(d)
 
-print(f)
+#print(f)
 
  # Решение задачи 3 урока 13
  
@@ -23,4 +23,43 @@ print(f)
 #for i in range(len(a)):
     #z = a[i]
     #print(z[0],'-',z[1],'-',z[2])
-    
+ 
+
+#Задача на вывод чисел из списка.
+ 
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#for DD in a:
+    #if DD < 5:
+        #print(DD)
+        
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#for DD in a:
+    #if DD > 5:
+        #print(DD)
+
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#print(list(filter(lambda elem: elem > 5, a)))
+
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#print([elem for elem in a if elem > 5])
+
+#Вернуть список, который состоит из элементов, общих для этих двух списков.
+ 
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+#b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55, 12, 13]
+
+#result = list(filter(lambda elem: elem in b, a)) 
+#print(result)
+
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+#b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55, 12, 13]
+#result = [elem for elem in a if elem in b]
+#print(result)
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55, 12, 13]
+result = list(set(a) & set(b))
+print(result)
+
+ 
