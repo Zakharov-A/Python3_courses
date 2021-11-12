6bc0d8ced3833399fc2186eb17ab736cf3d7bb35
root <Zakharov-A>
2021-11-12 10:30:47 -0500

    My_Py_selfedu_old_less14

--- Selfedu_old_course/Less 14 функции (def) - объявление и вызов (1).py
+++ Selfedu_old_course/Less 14 функции (def) - объявление и вызов (1).py
@@ -1,28 +1,48 @@
 #def sayHello():
     #print("hello")
-
-    #sayHello()
+#sayHello()
 #print("----------------")
 #sayHello()
+
 #--------------------------------------    
 #def sayHello():
     #print("hello")
 
 #def myAbs(x):
     #x = -x if(x<0) else x
-    #print(x)
     #return x
-
 #print( myAbs(-5) )
 #print( myAbs(15) )
 #a = 100
 #print( myAbs(a) )
 
-#--------------
 
+#val = myAbs(-5.8)
+#print(val)
+
+#def isPositive(x):
+#    if x >= 0:
+#        return x >= 0
+#p = []
+#for a in range(-5, 11):
+#    if isPositive(a):
+#        p.append(a)
+#print(p)
 
 #def sayHello(msg, end="!", sep=": "):
-    #print("Message"+sep+msg+end)
+#    print("Message"+sep+msg+end)
+    
+#def getSquare(w, h):
+#        return 2*(w+h)
+#p = getSquare(10, 5.5)        
+#print(p)
+#sayHello("Hello", sep=" ")
+
+#def perAndSq(w, h):
+#    return 2*(w+h), w*h
+#per, sq = perAndSq(2.3, 5)
+#print(per, sq)
+
     
 #def myAbs(x):
     #x = -x if(x<0) else x
@@ -38,21 +58,25 @@
 # Следующий пример
 
 #def myPow(x, n):
-    #sx = 1
-    #while n >0:
-        #sx *= x
-        #n -= 1
-    #return sx
+#    sx = 1
+#    while n >0:
+#        sx *= x
+#        n -= 1
+#    return sx
+#p = myPow(3, 5)
+#print(p)
+
 
 #def sayHello():
-    #print("hello")
+#    print("hello")
 
 #def sayHello():
-    #print("-----hello-----")
+#    print("-----hello-----")
+#sayHello()    
 
 #def sayHello(msg):
-    #print(msg)    
-
+#    print(msg)    
+#sayHello("qwerty")  
 
 #sayHello("redred")
 
@@ -62,13 +86,24 @@
 #TYPE_FUNC = False
 
 #if TYPE_FUNC:
-    #def sayHello():
-        #print("hello")
+#    def sayHello():
+#        print("hello")
 #else:
-    #def sayHello(msg):
-        #print(msg)
+#    def sayHello(msg):
+#        print(msg)
         
-#sayHello("Ha ha ha")
+#sayHello(123)
+
+# Следующий пример
+#def max2(a, b):
+#   if a>b:
+ #       return a
+  # else:
+   #     return b
+
+#print( max2(2, -3) )
+
+
 
 def max2(a, b):
     if a>b:
@@ -78,9 +113,10 @@ def max2(a, b):
 def max3(a, b, c):
     return max2(a, max2(b, c))
     
-print( max3(2, -3, 5) )
-print( max3("ab", "cd", "abc"))
+print( max3(22, -3, 5) )
+print( max3("ab", "cdb", "abc"))
 
+#Задания:
 
  
 
