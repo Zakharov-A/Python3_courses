03681ad6a7d069fd22563e85c685bc6a5a357af4
Zakharov-A <redzed427@gmail.com>
2021-11-26 08:21:50 -0500

    Python_selfedu_less_15_game_sapper

--- Selfedu_old_course/Less 15 делаем Сапер проектирование программ сверху-вниз.py
+++ Selfedu_old_course/Less 15 делаем Сапер проектирование программ сверху-вниз.py
@@ -14,9 +14,10 @@ def getTotalMines(PM, i, j):
                 n += 1
     return n                
             
+     
 def createGame(PM):
     """Создание игрового поля: расположение мин
-    и посчет числа мин вокруг клеток без мин
+       и посчет числа мин вокруг клеток без мин
     """
     rng = random.Random()
     
@@ -28,13 +29,13 @@ def createGame(PM):
             continue
         PM[i*N+j] = -1
         n -= 1
+        
     # вычисляем количество мин вокруг клетки
     for i in range(N):
         for j in range(N):
-            if PM[i*N+j] == 0:
+           if PM[i*N+j] == 0:
                 PM[i*N+j] = getTotalMines(PM, i, j)
         
-
 def show(pole):
     """Функция отображениясостояния текущего
        игрового поля 
@@ -43,27 +44,28 @@ def show(pole):
         for j in range(N):
             print( str(pole[i*N+j]).rjust(3), end="" )
         print()
-        
+
 def goPlayer():
-    """Функция отображения состояния текущего
-    игрового поля 
+    """Функция отображения состояния
+    текущего игрового поля 
     """
     flLoopInput = True
     while flLoopInput:
-        x, y = input("Введите координату через пробел: ").split()
+        x, y = input("Введите через пробел 2 координаты: ").split()
         if not x.isdigit() or not y.isdigit():
-            print("Координаты введены неверно")
+            print("Координаты введены не верно")
             continue
+
         x = int(x)-1
         y = int(y)-1
 
-        if x < 0 or x >=N or y < 0 or y >= N:
-           print("Координаты выходят за пределы поля")
-           continue
+        if x < 0 or x >= N or y < 0 or y >= N:
+            print("Координаты выходят за приделы поля ")
+            continue
 
         flLoopInput = False
-    return (x,y)        
-
+    return(x,y)            
+        
 def isFinish(PM, P):
     """Определение  текущего состояния игры:
        выиграли, проиграли, игра продолжается 
@@ -71,27 +73,27 @@ def isFinish(PM, P):
     for i in range(N*N):
         if P[i] != -2 and PM[i] < 0: return -1
 
-    for i in range(N*N):        
-        if P[i] == -2 and PM[i] >=0: return 1
+    for i in range(N*N):    
+        if P[i] == -2 and PM[i] >= 0: return 1
+    return -2    
         
-    return -2
-
 def startGame():
     """Функция запуска игры: отображает игровое поле,
-       игрок откр. любую клетку, резул. проверяется на
+       игрок открывает любую клетку, результат проверяется на
        наличие мины или выиграшной ситуации
     """
     P = [-2]*N*N
     PM = [0]*N*N
     
     createGame(PM)
-   
+    
     finishState = isFinish(PM, P)
-    while finishState > 0:
+    while isFinish(PM, P) > 0:
         show(P)
         x,y = goPlayer()
         P[x*N+y] = PM[x*N+y]
         finishState = isFinish(PM, P)
+        
     return finishState
 
 res = startGame()
@@ -99,7 +101,13 @@ if res == -1:
     print("Вы проиграли")
 else:
     print("Вы выиграли")
+    
+print("Игра завершена")
+
+    
+    
+
         
 
-print("Игра завершина")
+
 
