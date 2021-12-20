cd52799e32e5686fe4ac70372965a1fccd3c77e8
Zakharov-A <redzed427@gmail.com>
2021-12-20 09:04:55 -0500

    Py_Selfedu_19_less

---
+++ Selfedu_old_course/Less_19_множества_и_операции_с_ними.py
@@ -0,0 +1,47 @@
+# a = {1,2,3,"hello",(1,2), ("a","b")}
+# print(a)
+#
+# b=set("Hello world!")
+# print(b)
+
+# a=[1,2,3,43,2,4,3,2,1]
+# b = set(a)
+# c = list(b)
+# print(c)
+
+# a=[1,2,3,43,2,4,3,2,1]
+# print(list(set(a)))
+
+# setA = {7,6,5,4,3}
+# for x in setA:
+#     print(x)
+
+# a={"abc", (1,2), 5, 4, True}
+# print(len(a))
+
+# setA = {1,2,3,4}
+# setB = {3,4,5,6,7}
+# print(setA | setB)
+
+# setA = {1,2,3,4}
+# setB = {3,4,5,6,7}
+# c = setA - setB
+# print(c)
+
+# setA = {1,2,3,4}
+# setB = {3,4,5,6,7}
+# c = setB - setA
+# print(c)
+
+# setA = {1,2,3,4}
+# setB = {3,4,5,6,7}
+# d = setA ^ setB
+# print(d)
+
+# setA = {1,2,3,4}; setB = {1,2,3,4,5,6,7}
+# d = setB > setA
+# print(d)
+
+# Задание
+
+print(len(set(input('Введи через пробел: ').split())))
