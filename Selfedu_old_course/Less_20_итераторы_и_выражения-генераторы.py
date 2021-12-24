03fd83fa15d6c176852dc3c73e52824561f5fa15
Zakharov-A <redzed427@gmail.com>
2021-12-23 21:59:28 -0500

    Py_Selfedu_20_less

---
+++ Selfedu_old_course/Less_20_итераторы_и_выражения-генераторы.py
@@ -0,0 +1,10 @@
+# lst = (x for x in range(1000000000))
+# for i in lst:
+#      print(i, end=" ")
+#      if i > 100: break
+#
+# print("\nnew \nloop")
+#
+# for i in lst:
+#      print(i, end=" ")
+#      if i > 200: break
