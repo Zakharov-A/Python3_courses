baa7c351de22731671d8d0ff0fda7d407e1adcd6
Zakharov-A <redzed427@gmail.com>
2021-12-26 18:17:53 -0500

    Py_Selfedu_20_less

--- Selfedu_old_course/Less_20_итераторы_и_выражения-генераторы.py
+++ Selfedu_old_course/Less_20_итераторы_и_выражения-генераторы.py
@@ -8,3 +8,33 @@
 # for i in lst:
 #      print(i, end=" ")
 #      if i > 200: break
+
+
+#---------
+
+
+def getAllAverage(N):
+     count = 0
+     S = 0
+     for i in range(1,N+1):
+          count += 1
+          S += i
+          yield S/count
+it = getAllAverage(10)
+print( next(it) )
+print( next(it) )
+print( next(it) )
+print( next(it) )
+print( next(it) )
+print( next(it) )
+print( next(it) )
+
+
+# def f ():
+#      for x in range(10):
+#           yield x
+# s = f()
+# print( s )
+# print( next(s) )
+# print( next(s) )
+# print( next(s) )
