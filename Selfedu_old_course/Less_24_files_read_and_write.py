01f899be809bb7ef2de469872828960c0c6dc86c
redzed <redzed427@gmail.com>
2022-01-05 23:00:54 +0300

    Py_selfedu_less_24

--- Selfedu_old_course/Less_24_files_read_and_write.py
+++ Selfedu_old_course/Less_24_files_read_and_write.py
@@ -5,7 +5,7 @@
 #     print( file.read(20) )
 #     pos = file.tell()
 #     print( pos )
-#
+
 # except FileNotFoundError:
 #     print("Невозможно открыть файл")
 
@@ -16,8 +16,7 @@
 #     s = file.readline()
 #     print( s )
 #     print( file.readline() )
-#     print( file.readline() )
-#     print( file.readline() )
+   
 # except FileNotFoundError:
 #     print("Невозможно открыть файл")
 
@@ -45,17 +44,122 @@
 #------------------------------------------------
 
 
+# try:
+#     file = open("myfile.txt")
+
+#     try:
+#         s = file.readlines()
+#         print(s)
+#     finally:
+#         file.close()
+
+# except FileNotFoundError:
+#     print("Невозможно открыть файл")
+
+#------------------------------------------
+
+# try:
+#     # file = open("myfile.txt")
+#     with open("myfile.txt", "r") as file:
+#         s = file.readlines()
+#         print(s)
+
+# except FileNotFoundError:
+#     print("Невозможно открыть файл")
+# finally:
+#     print(file.closed)    
+
+#----------------------------
+
+# try:
+#     file = open("out.txt", "w")
+
+#     try:
+#         file.write("Hello Hello! Faka!1\n")
+#         file.write("Hello Hello! Faka!2\n")
+#         file.write("Hello Hello! Faka!3\n")
+#     finally:
+#         file.close()
+
+# except FileNotFoundError:
+#     print("Невозможно открыть файл")
+
+#-----------------------
+
+# try:
+#     file = open("out.txt", "a+")
+
+#     try:
+#         file.write("Hello Hello! Faka!1\n")
+#         file.write("Hello Hello! Faka!2\n")
+#         file.write("Hello Hello! Faka!3\n")
+#         file.seek(0)
+#         print( file.read() )
+#     finally:
+#         file.close()
+
+# except FileNotFoundError:
+#     print("Невозможно открыть файл")
+
+#-------------------------
+
+# try:
+#     file = open("out.txt", "w")
+
+#     try:
+#         file.writelines(["Hello Faka1\n", "Hello2\n"])
+#     finally:
+#         file.close()
+
+# except FileNotFoundError:
+#     print("Невозможно открыть файл")
+
+#----------------
+
+# import pickle
+
+# books = [
+# ("А сороки-Белобоки"),
+# ("Поскакали по полям,"),
+# ("Закричали журавлям:"),
+# ("«Горе! Горе! Крокодил"),
+# ("Солнце в небе проглотил!»")
+# ]
+
+# try:
+#     file = open("out.txt", "rb")
+
+#     try:
+#         # pickle.dump(books, file)
+#         bs = pickle.load(file)
+#         print( bs )
+#     finally:
+#         file.close()
+
+# except FileNotFoundError:
+#     print("Невозможно открыть файл")
+
+#--------------------------
+
+import pickle
+
+books = [
+("А сороки-Белобоки"),
+("Поскакали по полям,"),
+("Закричали журавлям:"),
+("«Горе! Горе! Крокодил"),
+("Солнце в небе проглотил!»")
+]
+
 try:
-    file = open("myfile.txt")
+    file = open("out.txt", "rb")
 
     try:
-        s = file.readlines()
-        print(s)
+        # pickle.dump(books, file)
+        bs = pickle.load(file)
+        print( bs )
     finally:
         file.close()
 
 except FileNotFoundError:
     print("Невозможно открыть файл")
-
-#------------------------------------------
-#-------Stop-Time------13:36---------------
