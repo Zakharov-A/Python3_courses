cadd1eb128f4f9609c3a7b91c47c2c1b1e56fd23
Zakharov-A <global user.email redzed427@gmail.com>
2022-02-13 16:28:51 +0300

    py_courses_less_7_for_while

---
+++ Best_Py/Py_Less_6.py
@@ -0,0 +1,52 @@
+# Тернальный оператор
+
+# data = input("Введите число прописью:")
+#
+# number = 5 if data == "Five" else 0
+# print(number)
+
+# for i in range(1, 6, 2):
+#     print(i)
+
+# count = 0
+# word = " Hello World!"
+# for i in word:
+#     if i == "W":
+#         count += 1
+# print("Count:", count)
+
+# i = 5
+# while i <= 15:
+#     print(i)
+#     i += 2
+
+# isHasCar = True
+#
+# while isHasCar:
+#     if input("enter the information:  ") == "Stop":
+#         isHasCar = False
+
+# for i in range(11):
+#     if i == 5:
+#         break
+#     print(i)
+
+# for i in range(11):
+#     if i == 5:
+#         break
+#     if i % 2 == 0:
+#         continue
+#
+#     print(i)
+found = None
+for i in "hello":
+    if i == "r":
+        found = True
+        break
+else:
+    found = False
+
+print(found)
+
+
+
