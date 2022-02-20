557ef5bef059f99ef1d04873fc16f8ca7cb2490b
Zakharov-A <global user.email redzed427@gmail.com>
2022-02-20 18:26:08 +0300

    Py_Best_less_16_modules.py

---
+++ Best_Py/Py_less_16_modules.py
@@ -0,0 +1,28 @@
+# import time
+#
+# time.sleep(3)
+# print("Hello")
+
+# import datetime as IT
+# print(IT.datetime.now().date())
+# print(IT.datetime.now().time())
+
+import datetime as d, sys, os, platform
+# from math import sqrt as s, ceil
+#
+# print(ceil(s(25)))
+
+# import mymodule as my
+#
+# print(my.name)
+# my.hello()
+
+# from mymodule import add_three_numbers as aDD
+#
+# print(aDD(5, 3, 6))
+
+import cowsay as cow
+cow.cow('What will you choose Nastenka, I love you or fuck you?')
+
+
+
