fa4a498b7da7f488356e38fef8854af2f8cfd9c6
Darth <redzed427@gmail.com>
2022-05-07 04:09:15 -0400

    PY_course_Mikhail_Flenov_L_6

---
+++ PY_Mikhail_Flenov/Lesson_6.py
@@ -0,0 +1,19 @@
+
+# def test(a):
+#     a = 20
+
+
+# par = 10
+# test(par)
+# print(par)
+
+# ----
+
+def testlist(a):
+    a[1] = 30
+
+param = [10, 20]  
+testlist(param)
+print(param)  
+
+# ----
