f3075933ffb61159eda529f70a4dfb9c54497497
Zakharov-A <redzed427@gmail.com>
2022-05-09 09:16:03 +0300

    Py_Right_Now_course_l_1

---
+++ PY/L_1.py
@@ -0,0 +1,11 @@
+# hey.....there is nothing. WAKE UP!!!
+
+# print("Hello World!")
+# print("I'm iron man!")
+# print("No, I am Tony Stark!")
+# print("No, I am Poppy!")
+# # We are like Python
+
+print(""" I am Iron Man.
+ No, I am Tony Stark.
+ No, I am Poppy.""")
---
+++ Python RIGHT NOW/L_1.py
@@ -0,0 +1,18 @@
+# hey.....there is nothing. WAKE UP!!!
+
+# print("Hello World!")
+# print("I'm iron man!")
+# print("No, I am Tony Stark!")
+# print("No, I am Poppy!")
+# We are like Python
+
+# ----
+
+# print(""" I am Iron Man.
+#  No, I am Tony Stark.
+#  No, I am Poppy.""")
+
+# ----
+
+print(" I am Iron Man. \n" + " No, I am Tony Stark \n" + " No, I am Poppy!")
+print(" I am Iron Man \n" * 10)
