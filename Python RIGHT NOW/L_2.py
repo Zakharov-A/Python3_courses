664a4b18b5da235a70a01b10816cc8d120b8fbb1
Zaharov Andrei <redzed427@gmail.com>
2022-05-10 10:24:53 +0300

    Py_course_right_now_L_2

---
+++ Python RIGHT NOW/L_2.py
@@ -0,0 +1,13 @@
+
+#Let's duild robot Barista!!!
+print("Hello, welcome to NetworkChuck Coffe!!!!")
+name = input("What is your name? \n")
+print("Hello " + name + ", thank you so much for coming in today!")
+
+menu = "Black Coffe, Espresso, Latte, Cuppucino"
+
+print(name + ", what would you like from our menu today? Here is what we are serving. \n" + menu)
+
+order = input()
+
+print("Sounds good " + name + ", we'll have that " + order + " ready for in a moment.")
