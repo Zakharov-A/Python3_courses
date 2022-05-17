0eda1cfdbeba0783b9f836a08ce4cf4d463ca190
Darth <redzed427@gmail.com>
2022-05-17 04:10:20 -0400

    PY_course_Python_RIGT_NOW_L_7_list

---
+++ Python RIGHT NOW/L_7.py
@@ -0,0 +1,22 @@
+#Hey, we're going on a camping trip!!!
+# here is the stuff we need
+
+# camping_stuff = "\ntent, sleeping bags, water, raspberry pi, coffee, knife, ethernet cable, flash drive, beard oil, marshmallows\n"
+# print(camping_stuff)
+
+#Python List
+
+camping_stuff = ["tent", "sleeping bags", "water", "raspberry pi", "coffee",
+"knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+print(camping_stuff)
+
+
+camp_site = ["Crystal Lake", 404, 98.3, True]
+
+print(camp_site)
+
+me = camping_stuff[4]
+you = camping_stuff[-1]
+print("Me: ", me)
+print("You: ", you)
