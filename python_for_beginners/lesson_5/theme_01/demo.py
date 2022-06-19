7b2920be969cdebd8afeda662d6c9ecae946f1c6
Zakharov-A <redzed427@gmail.com>
2022-06-19 18:48:43 +0300

    Python_for_beggines_lesson_5_part_1

---
+++ python_for_beginners/lesson_5/theme_01/demo.py
@@ -0,0 +1,13 @@
+# amount = 0
+# for _ in range(5):
+#     num = int(input())
+#     amount += num
+# print(amount)
+
+# ----
+
+amount = 0
+for _ in range(5):
+    num = int(input())
+    amount -= num
+print(amount)
