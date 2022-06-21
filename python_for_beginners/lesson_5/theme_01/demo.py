4b25ec604b580f448445ae93926a8ed4eee70816
Darth <redzed427@gmail.com>
2022-06-21 04:09:15 -0400

    Python_for_beginners_course_lesson_5_part_1-2

--- python_for_beginners/lesson_5/theme_01/demo.py
+++ python_for_beginners/lesson_5/theme_01/demo.py
@@ -1,13 +1,25 @@
+
+
+
+# for x in range(6):
+#     print(x)
+
 # amount = 0
 # for _ in range(5):
 #     num = int(input())
 #     amount += num
+#
 # print(amount)
 
 # ----
 
-amount = 0
-for _ in range(5):
-    num = int(input())
-    amount -= num
-print(amount)
+# amount = 0
+# for _ in range(5):
+#     num = int(input())
+#     amount -= num
+# print(amount)
+
+for i in range(6):
+    if i % 2 == 0:
+        print(i)
+
---
+++ python_for_beginners/lesson_5/theme_02/range_function_parameters.py
@@ -0,0 +1,6 @@
+
+# for i in range(-3, 3, 2):
+#     print(i)
+
+for i in range(6, -4, -2):
+    print(i)
