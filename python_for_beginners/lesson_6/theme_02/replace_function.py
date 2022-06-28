8e7c8cccfc5d9446f5219a60ec21bd3448b953c7
Darth <redzed427@gmail.com>
2022-06-28 03:02:01 -0400

    Python_for_beginners_course_lesson_6_part_2

---
+++ python_for_beginners/lesson_6/theme_02/replace_function.py
@@ -0,0 +1,21 @@
+# password -> crossword
+
+# x = 'password'.replace('pass', 'cross')
+# print(x)
+
+# ----
+
+# print('password'.replace('pass', 'cross'))
+
+# ----
+
+# x = 'Hello' in 'Hello world'
+# print(x)
+
+# ----
+
+print('word'.isalpha())
+print('word'.isdigit())
+print('word'.isalnum())
+
+
