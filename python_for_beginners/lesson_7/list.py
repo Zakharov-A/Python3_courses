54380c66290f1365d040de20a6b703ba0f927015
Zakharov-A <redzed427@gmail.com>
2022-06-30 22:48:15 +0300

    Python_for_beggines_lesson_7_1

---
+++ python_for_beginners/lesson_7/list.py
@@ -0,0 +1,53 @@
+# list_1 = [1, 1.5, '1', 'abc', True]
+# print(list_1)
+# print(list_1[1])
+#
+# text = '1, 2, 3, 4, 5'
+# print(text.split(','))
+#
+# text_1 = '1 2 3 4 5'
+# print(text.split(' '))
+
+# ----
+
+# text = '1 2 3 4 5'
+# list_3 = text.split()
+# print(list_3)
+#
+# for i in range(len(list_3)):
+#     list_3[i] = int(list_3[i])
+#
+# print(list_3)
+#
+# # ----
+#
+# text = '1 2 3 4 5'
+# list_4 = text.split()
+# print(list_4)
+#
+# for idx, item in enumerate(list_4):
+#     list_4[idx] = int(item)
+#
+# print(list_4)
+
+# ----
+
+text = '1 2 3 4 5'
+list_5 = text.split()
+print(list_5)
+
+for item in list_5:
+    print(item, end=', ')
+
+
+
+
+
+
+
+
+
+
+
+
+
